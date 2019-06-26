16812c57c009905814bb73203f3d8ebf54b8f83e
Brian Plantico <bplantico@gmail.com>
2019-06-25 18:51:57 -0600

    Adds merchants index and show routes and controller which are both serialized

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,7 +8,19 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body)
-    
+
     expect(merchants["data"].count).to eq(3)
+  end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["id"]).to eq(id)
+    # circle back. If <["attributes"]> is removed, the return is a string which fails the test
+    
   end
 end
