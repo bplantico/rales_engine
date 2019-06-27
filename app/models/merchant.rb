89cc1761a7b3e0529fde20d09d8b31e7e4b5423d
Brian Plantico <bplantico@gmail.com>
2019-06-26 19:05:17 -0600

    Adds case of quantity not existing for .most_revenue Merchant method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,11 +5,18 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def self.most_revenue(quantity)
-    joins(invoices: :invoice_items)
-      .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-      .group("merchants.id")
-      .order("revenue DESC")
-      .limit(quantity)
+    if quantity.to_i >= 1
+      joins(invoices: :invoice_items)
+        .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+        .group("merchants.id")
+        .order("revenue DESC")
+        .limit(quantity)
+      else
+        joins(invoices: :invoice_items)
+          .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+          .group("merchants.id")
+          .order("revenue DESC")
+      end
   end
 
 end
