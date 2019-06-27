798818dce56894c163db3c92a4f67e9a4cb1a141
Brian Plantico <bplantico@gmail.com>
2019-06-26 18:50:37 -0600

    Adds Merchant.most_revenue(quantity) to merchant class

---
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -0,0 +1,15 @@
+# app/controllers/api/v1/merchants/most_revenue_controller.rb
+
+class Api::V1::Merchants::MostRevenueController < ApplicationController
+
+  def index
+    render json: MerchantSerializer.new(Merchant.most_revenue(revenue_params[:quantity]))
+  end
+
+  private
+
+  def revenue_params
+    params.permit(:quantity)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/most_revenue', to: 'most_revenue#index'
+      end
       resources :merchants, only: [:index, :show]
     end
   end
